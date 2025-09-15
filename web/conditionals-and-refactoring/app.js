804b1a9c76c6b2018d172d9e264c77b02b6dfd32
Shirley Xue <shirley_xue@g.harvard.edu>
2025-09-15 11:48:34 -0400

    conditionals and refactoring; input and output

---
+++ web/conditionals-and-refactoring/app.js
@@ -0,0 +1,9 @@
+let response = prompt('What is 4+6?');
+
+if (response == 10) {
+    alert('You got it correct!')
+} else if (response > 8 && response < 12) {
+    alert('You almost got it correct.')
+} else {
+    alert('You got it incorrect.')
+}
